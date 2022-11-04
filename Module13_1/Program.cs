5e9f74baf91a4d88a582a7853fe1f79508e513a0
pavelsannikov <pavelsan95@mail.ru>
2022-11-04 13:16:34 +0500

    Добавьте файлы проекта.

---
+++ Module13_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string all_text = File.ReadAllText("Text1.txt");
+        char[] delimiters = new char[] { ' ', '\r', '\n' };
+        var words = all_text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+        List<string> words_list = new List<string>(words);
+        LinkedList<string> words_linkedlist = new LinkedList<string>(words);
+
+        Random rnd = new Random();
+        int num_measure = 10000;
+
+        var watch = Stopwatch.StartNew();
+        for (int i = 0; i < num_measure; i++)
+        {
+            words_list.Insert(rnd.Next(0, words_list.Count), "MyText");
+        }
+        Console.WriteLine("Время выполения {1} случайных вставок в List: {0}", watch.Elapsed, num_measure);
+
+        watch = Stopwatch.StartNew();
+        for (int i = 0; i < num_measure; i++)
+        {
+            int index = rnd.Next(0, words_linkedlist.Count);
+            LinkedListNode<string> node = words_linkedlist.First;
+            for (int j = 0; j < index; j++) node = node.Next;
+            words_linkedlist.AddAfter(node, "MyText");
+        }
+        Console.WriteLine("Время выполения {1} случайных вставок в LinkedList: {0}", watch.Elapsed, num_measure);
+    }
+
+}
---
+++ Module13_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string all_text = File.ReadAllText("Text1.txt");
+        char[] delimiters = new char[] { ' ', '\r', '\n' };
+        var noPunctuationText = new string(all_text.Where(c => !char.IsPunctuation(c)).ToArray());
+        var words = noPunctuationText.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+        Dictionary<string, int> words_dict = new Dictionary<string, int>();
+        foreach (string word in words)
+        {
+            if (words_dict.ContainsKey(word))
+            {
+                words_dict[word]++;
+            }
+            else
+            {
+                words_dict.Add(word, 1);
+            }
+        }
+        var words_dict_sorted = words_dict.OrderByDescending(word => word.Value);
+        for (int i = 0; i < 10; i++)
+            Console.WriteLine("{0}:\t{1}", words_dict_sorted.ElementAt(i).Key, words_dict_sorted.ElementAt(i).Value);
+    }
+
+}
